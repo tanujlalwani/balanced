12e078395ecdbc9131f14d966a563e78f6f0d1d2
Tanuj Lalwani <tanuj.lalwani@protonmail.com>
2020-02-05 20:49:19 -0800

    Modify balanced to support "{", "}", "[", "]"

--- balanced.js
+++ balanced.js
@@ -1,10 +1,15 @@
 module.exports = function balanced(str) {
   let stack = [];
+  let brackets = {
+    '(': ')',
+    '[': ']',
+    '{': '}'
+  };
 
   for (let i = 0; i < str.length; i++) {
-    if (str[i] === '(') {
-      stack.push(')');
-    } else {
+    if (str[i] === '(' || str[i] === '[' || str[i] === '{') {
+      stack.push(brackets[str[i]]);
+    } else if (str[i] === ')' || str[i] === ']' || str[i] === '}') {
       let popped = stack.pop();
       if (popped !== str[i]) return false;
     }
