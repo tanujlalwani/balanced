ce4c57668aed671e048053b9d69615ef2cadee72
Tanuj Lalwani <tanuj.lalwani@protonmail.com>
2020-02-05 20:58:05 -0800

    Add tests to check mixed brackets

--- test/test.js
+++ test/test.js
@@ -2,11 +2,15 @@ var balanced = require('../balanced');
 var assert = require('assert');
 
 describe('#balanced()', function() {
+  // Basic parentheses
   it('check if brackets are balanced', function() {
-    assert.equal(balanced('()'), true);
+    assert.equal(balanced(''), true);
   });
   it('check if brackets are balanced', function() {
-    assert.equal(balanced('())'), false);
+    assert.equal(balanced('(()())'), true);
+  });
+  it('check if brackets are balanced', function() {
+    assert.equal(balanced('()('), false);
   });
   it('check if brackets are balanced', function() {
     assert.equal(balanced(')()'), false);
@@ -14,10 +18,38 @@ describe('#balanced()', function() {
   it('check if brackets are balanced', function() {
     assert.equal(balanced('(((((((((('), false);
   });
+
+  // Mixed parentheses
   it('check if brackets are balanced', function() {
-    assert.equal(balanced('(()())'), true);
+    assert.equal(balanced('{}([])'), true);
   });
-  // it('check if brackets are balanced', function() {
-  //   assert.equal(balanced('{}([])'), true);
-  // });
+  it('check if brackets are balanced', function() {
+    assert.equal(balanced('[{}([])]()'), true);
+  });
+  it('check if brackets are balanced', function() {
+    assert.equal(balanced('{{[(])}}'), false);
+  });
+  it('check if brackets are balanced', function() {
+    assert.equal(balanced('}{[]}}'), false);
+  });
+  it('check if brackets are balanced', function() {
+    assert.equal(balanced('([](()()){[]((((()))))})'), true);
+  });
+
+  // Mixed parenthesis + other chars
+  it('check if brackets are balanced', function() {
+    assert.equal(balanced('(abc (def (hij)))'), true);
+  });
+  it('check if brackets are balanced', function() {
+    assert.equal(balanced('[(a^2) + (b^2) - 2 * (5-(2/(2+3)))]'), true);
+  });
+  it('check if brackets are balanced', function() {
+    assert.equal(balanced('(((a[bc(])))'), false);
+  });
+  it('check if brackets are balanced', function() {
+    assert.equal(balanced('abc(yuiop)(((((([234]{q}))))))zxc'), true);
+  });
+  it('check if brackets are balanced', function() {
+    assert.equal(balanced('{[()](asd)}'), true);
+  });
 });
